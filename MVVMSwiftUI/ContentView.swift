7466cb20272627a60f06e20d08f584a6faeaf405
Bryan Caro <bryancaro96@gmail.com>
2022-09-08 12:43:24 +0200

    Initial Commit

---
+++ MVVMSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVMSwiftUI
+//
+//  Created by Bryan Caro on 8/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVMSwiftUI/MVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUIApp.swift
+//  MVVMSwiftUI
+//
+//  Created by Bryan Caro on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
