7d848fb63212ec82fa50f3e579beeb8f9c66e088
Bryan Caro <bryancaro96@gmail.com>
2022-09-08 13:21:12 +0200

    implement UI Test

--- MVVMSwiftUITests/MVVMSwiftUITests.swift
+++ MVVMSwiftUITests/MVVMSwiftUITests.swift
@@ -8,6 +8,12 @@
 import XCTest
 @testable import MVVMSwiftUI
 
+/*
+    Naming Structure: test_UnitOfWork_StateUnderTest_ExpectedBehavior
+ 
+    Testing Structure: Given, When, Then
+ */
+
 class MVVMSwiftUITests: XCTestCase {
 
     override func setUpWithError() throws {
@@ -17,20 +23,26 @@ class MVVMSwiftUITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_ContentViewModel_isLoading_shouldBeTrue() {
+        //  Given
+        //  Loading is true by default
+        
+        //  When
+        let vm = ContentView.ViewModel()
+        
+        //  Then
+        XCTAssertTrue(vm.isLoading)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_ContentViewModel_isLoading_shouldBeFalse() {
+        //  Given
+        //  Loading is true by default
+        
+        //  When
+        let vm = ContentView.ViewModel()
+        
+        //  Then
+        XCTAssertFalse(vm.isLoading)
     }
-
 }
